9692eb580cf2b17ef14e7449c400ac5e43b37284
Dorian190 <158452741+Dorian190@users.noreply.github.com>
2024-04-11 10:59:49 +0200

    Add files via upload

---
+++ ispit zadatak 2727.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Vozilo
+{
+    public bool JeLiLeti { get; set; }
+    public bool JeLiPlovi { get; set; }
+
+    public void KudaVozi()
+    {
+        Console.WriteLine($"Ovo vozilo {(JeLiLeti ? "leti" : "ne leti")} i {(JeLiPlovi ? "plovi" : "ne plovi")}.");
+    }
+}
+
+public class Brod : Vozilo
+{
+    public Brod()
+    {
+        JeLiLeti = false;
+        JeLiPlovi = true;
+    }
+}
+
+
+public class Zrakoplov : Vozilo
+{
+   
+    public Zrakoplov()
+    {
+        JeLiLeti = true;
+        JeLiPlovi = false;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Brod brod = new Brod();
+        Zrakoplov zrakoplov = new Zrakoplov();
+
+        Console.WriteLine("Brod:");
+        Console.WriteLine($"Je li leti: {brod.JeLiLeti}");
+        Console.WriteLine($"Je li plovi: {brod.JeLiPlovi}");
+        brod.KudaVozi();
+
+        Console.WriteLine("\nZrakoplov:");
+        Console.WriteLine($"Je li leti: {zrakoplov.JeLiLeti}");
+        Console.WriteLine($"Je li plovi: {zrakoplov.JeLiPlovi}");
+        zrakoplov.KudaVozi(); 
+    }
+}
